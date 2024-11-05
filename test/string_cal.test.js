864c8d1725268f272be9c8bb362e991f673ed817
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-11-06 02:25:56 +0530

    Add failing test to handle new lines between numbers (instead of commas)

--- test/string_cal.test.js
+++ test/string_cal.test.js
@@ -17,3 +17,12 @@ describe("2: Allow the add method to handle an unknown amount of numbers",()=>{
         expect(add("1,2,3,4,5")).toBe(15);
     });
 });
+
+describe("3: Allow the add method to handle new lines between numbers (instead of commas)",()=>{
+    test("add('1\\n2,3') should return 6",()=>{
+        expect(add("1\n2,3")).toBe(6);
+    });
+    test("add('1\\n2,3\\n4') should return 10",()=>{
+        expect(add("1\n2,3\n4")).toBe(10);
+    });
+});
