fcf9e72106665a6db5c8f22d3dcd59847f523bdb
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-11-06 02:13:28 +0530

    Change the function name from string_cal to add for better clarity and convenience.

--- test/string_cal.test.js
+++ test/string_cal.test.js
@@ -1,13 +1,13 @@
-const string_cal=require("../model/string_cal");
+const add=require("../model/string_cal");
 
 describe("1: Create a simple string calculator",()=>{
     test("string_cal('') should return 0",()=>{
-        expect(string_cal("")).toBe(0);
+        expect(add("")).toBe(0);
     });
     test("string_cal('1') should return 1",()=>{
-        expect(string_cal("1")).toBe(1);
+        expect(add("1")).toBe(1);
     });
     test("string_cal('1,5') should return 6",()=>{
-        expect(string_cal("1,5")).toBe(6);
+        expect(add("1,5")).toBe(6);
     });
 });
