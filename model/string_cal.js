3c53bcbe00ad265bc39879419e8ff8b031ab51d5
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-11-06 03:10:21 +0530

    Implement logic to throw an exception when user input contains negative numbers task 5

--- model/string_cal.js
+++ model/string_cal.js
@@ -10,16 +10,30 @@ function add(str){
         str=str.substring(4);
         let numbers=str.split(delimiter);
         let sum=0;
+        let negative=[]
         for(let i=0;i<numbers.length;i++){
+            if(numbers[i][0]==="-"){
+                negative.push(numbers[i]);
+            }
             sum+=parseInt(numbers[i]);
+        }
+        if(negative.length>0){
+            throw new Error("negative numbers not allowed "+negative.join(","));
         }
         return sum;
     }
     else{
         let sum=0;
         let arr=str.split(/[\n,]/);
+        let negative=[]
         for(let i=0;i<arr.length;i++){
+            if(arr[i][0]==="-"){
+                negative.push(arr[i]);
+            }
             sum+=parseInt(arr[i]);
+        }
+        if(negative.length>0){
+            throw new Error("negative numbers not allowed "+negative.join(","));
         }
         return sum;
     }
