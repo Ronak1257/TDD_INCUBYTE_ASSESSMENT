e15b4e694ffb267bffcbe992d10d14138357d60b
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-11-06 02:18:55 +0530

    Rename the test to add for clarity and consistency with the function name.

--- test/string_cal.test.js
+++ test/string_cal.test.js
@@ -1,13 +1,13 @@
 const add=require("../model/string_cal");
 
 describe("1: Create a simple string calculator",()=>{
-    test("string_cal('') should return 0",()=>{
+    test("add('') should return 0",()=>{
         expect(add("")).toBe(0);
     });
-    test("string_cal('1') should return 1",()=>{
+    test("add('1') should return 1",()=>{
         expect(add("1")).toBe(1);
     });
-    test("string_cal('1,5') should return 6",()=>{
+    test("add('1,5') should return 6",()=>{
         expect(add("1,5")).toBe(6);
     });
 });
