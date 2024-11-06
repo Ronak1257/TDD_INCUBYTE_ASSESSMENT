a70921784d076611ceb32deb9e6dc50ff53bbbe5
Ronak Bharadiya <ronakbharadiya1257@gmail.com>
2024-11-06 22:51:01 +0530

    Optimize the code for further integrations

--- model/string_cal.js
+++ model/string_cal.js
@@ -1,41 +1,30 @@
+function num_add(numbers){
+    let sum=0;
+    let negative=[]
+    for(let i=0;i<numbers.length;i++){
+        if(numbers[i][0]==="-"){
+            negative.push(numbers[i]);
+        }
+        sum+=parseInt(numbers[i]);
+    }
+    if(negative.length>0){
+        throw new Error("negative numbers not allowed "+negative.join(","));
+    }
+    return sum;
+}
+
 function add(str){
-    if(str===""){
-        return 0;
-    }
-    else if(str.length==1){
-        return parseInt(str);
-    }
+    if(str==="") return 0;
+    else if(str.length==1) return parseInt(str);
     else if(str[0]==='/' && str[1]==='/'){
         let delimiter=str[2];
         str=str.substring(4);
         let numbers=str.split(delimiter);
-        let sum=0;
-        let negative=[]
-        for(let i=0;i<numbers.length;i++){
-            if(numbers[i][0]==="-"){
-                negative.push(numbers[i]);
-            }
-            sum+=parseInt(numbers[i]);
-        }
-        if(negative.length>0){
-            throw new Error("negative numbers not allowed "+negative.join(","));
-        }
-        return sum;
+        return num_add(numbers);
     }
     else{
-        let sum=0;
-        let arr=str.split(/[\n,]/);
-        let negative=[]
-        for(let i=0;i<arr.length;i++){
-            if(arr[i][0]==="-"){
-                negative.push(arr[i]);
-            }
-            sum+=parseInt(arr[i]);
-        }
-        if(negative.length>0){
-            throw new Error("negative numbers not allowed "+negative.join(","));
-        }
-        return sum;
+        let numbers=str.split(/[\n,]/);
+        return num_add(numbers);
     }
 }
 
